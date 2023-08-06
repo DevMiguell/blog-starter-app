5be9930bf828525820743e3bf8782d0c71f5b3b3
DevMiguell <buguel2000@gmail.com>
2023-08-05 21:15:19 -0300

    updates

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -1,12 +1,13 @@
-import { parseISO, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale';
+import { parseISO, format } from 'date-fns';
 
 type Props = {
-  dateString: string
-}
+  dateString: string;
+};
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
-}
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "d 'de' LLLL 'de' yyyy", { locale: ptBR })}</time>;
+};
 
-export default DateFormatter
+export default DateFormatter;
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -22,10 +22,7 @@ const PostPreview = ({
   slug,
 }: Props) => {
   return (
-    <div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
+    <div className="bg-[#00000029] p-8 rounded-3xl">
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
           as={`/posts/${slug}`}
@@ -39,7 +36,6 @@ const PostPreview = ({
         <DateFormatter dateString={date} />
       </div>
       <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }
